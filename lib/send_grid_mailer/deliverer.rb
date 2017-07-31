39f7b918655e182be69066ff52aad25ce2967fb9
Luis Vasconcellos <luis@luisvasconcellos.com>
2017-07-31 15:17:20 -0300

    Add multiple country support

--- lib/send_grid_mailer/deliverer.rb
+++ lib/send_grid_mailer/deliverer.rb
@@ -12,13 +12,21 @@ module SendGridMailer
     private
 
     def sg_api
-      @sg_api ||= Api.new(api_key)
+      @sg_api ||= Api.new(api_keys[current_key])
     end
 
-    def api_key
-      Rails.application.config.action_mailer.sendgrid_settings[:api_key]
+    def api_keys
+      {
+        br: ENV.fetch('SENDGRID_API_KEY_BR'),
+        mx: ENV.fetch('SENDGRID_API_KEY_MX'),
+        ar: ENV.fetch('SENDGRID_API_KEY_AR')
+      }
     rescue
       nil
+    end
+
+    def current_key
+      Countries.current.iso_2_code.downcase.to_sym
     end
   end
 end
